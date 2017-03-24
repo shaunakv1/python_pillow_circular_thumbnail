96f2af1370ca35dc09f3598b1f479f4df8bfe9a0
shaunakv1 <shaunakv1@gmail.com>
2017-03-24 02:09:45 -0400

    code example to show how to create circular thumbnail and overlap it on a background

---
+++ circular_thumbnail.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageOps, ImageDraw
+
+im = Image.open('avatar.jpg')
+im = im.resize((120, 120));
+bigsize = (im.size[0] * 3, im.size[1] * 3)
+mask = Image.new('L', bigsize, 0)
+draw = ImageDraw.Draw(mask) 
+draw.ellipse((0, 0) + bigsize, fill=255)
+mask = mask.resize(im.size, Image.ANTIALIAS)
+im.putalpha(mask)
+
+output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
+output.putalpha(mask)
+output.save('output.jpg')
+
+background = Image.open('back.jpg')
+background.paste(im, (150, 10), im)
+background.save('overlap.png')
